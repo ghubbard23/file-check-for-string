135cb33a9f6cb0dc5bc7a225f0b8643ed1f606e9
ghubbard23 <ghubbard23@gmail.com>
2017-03-07 11:39:40 -0600

    Add files via upload

--- file check v2.py
+++ file check v2.py
@@ -3,15 +3,22 @@ import os
 for root, dirs, files in os.walk(".", topdown=False):
      for file in files:
         with open(os.path.join(root, file), "r") as auto:
-            if '.log' in file:
+            if '.log' or '.LOG' in file:
+                print(file)
                 if 'Cisco IOS Software, IOS-XE Software, Catalyst 4500 L3 Switch  Software (cat4500es8-UNIVERSALK9NPE-M), Version 03.06.04.E RELEASE SOFTWARE (fc2)' in open (os.path.join(root, file)).read():
-                    print('true - BOOT ' + file)
+                    print('IOS - Good')
                 else:
-                    print('false - BOOT ' + file)
+                    print('IOS - Bad')
                 if 'ROM: 15.1(1r)SG5' in open (os.path.join(root, file)).read():
-                    print('true - ROMMON ' + file)
+                    print('ROMMON - Good')
                 else:
-                    print('false - ROMMON' + file)
+                    print('ROMMON - Bad')
+                if 'BOOT variable = bootflash:cat4500es8-universalk9npe.SPA.03.06.04.E.152-2.E4.bin,1;' in open (os.path.join(root, file)).read():
+                    print('Boot Var - Good')
+                else:
+                    print('Boot Var - Bad')
+                print('\n')
+                          
 
             
 
